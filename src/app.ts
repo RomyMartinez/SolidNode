b4617bf8e31eb1d9c6f7d946b3d46d2568e07a37
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-09-05 22:22:09 -0300

    config installing Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '../generated/prisma/client'
+
+export const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Romy',
+    email: 'romy@example.com',
+  },
+})
 
 export const app = fastify()
