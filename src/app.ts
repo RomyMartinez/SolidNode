348aab6cecac9afc57ee80f0a8515d89a4240dbf
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-09-06 01:25:44 -0300

    first migration

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,4 @@ import { PrismaClient } from '../generated/prisma/client'
 
 export const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    name: 'Romy',
-    email: 'romy@example.com',
-  },
-})
-
 export const app = fastify()
